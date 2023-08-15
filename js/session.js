a7ee76a9eaed0220477616bd9cadbcfee7d6d309
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-08-15 13:19:46 +0300

    session code

---
+++ js/session.js
@@ -0,0 +1,69 @@
+console.log("I am external");
+
+
+// One line comment
+
+/*   Multi line comment */
+
+
+/*
+var x;
+
+x = 50;
+*/
+var x = 50,
+    y = 60,
+    z = x + y;
+
+console.log(z);
+
+
+
+
+
+// String
+var username = "Mohamed Ahmed";
+
+console.log(username)
+
+//Number
+var user_id = 60;
+
+console.log(user_id)
+
+// Boolean
+var valid = true;
+
+console.log(valid)
+
+// Undefined
+
+var q;
+
+console.log(q)
+
+var division = document.getElementById("test");
+
+console.log(division)
+
+
+/***************************************/
+
+var firstName = "Osman",
+    lastName = "Ali";
+
+console.log(firstName + "     " + lastName);
+
+
+/*************************************
+
+var firstName2 = +prompt("Please enter your first name"),
+    lastName2 = +prompt("Please enter your last name")
+
+console.log(firstName2 + lastName2)
+
+
+var age = prompt("Please enter your age");
+
+console.log("Your age in days is : "+age * 365)
+**/
