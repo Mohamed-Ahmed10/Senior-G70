5d0424fa3c42b8c59b93db3520c6e2902a9038a7
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-08-17 13:54:53 +0300

    session code

--- js/session.js
+++ js/session.js
@@ -1,69 +1,126 @@
-console.log("I am external");
+var car = {
+    model: 2023,
+    year: '1985',
+    company: "BMW",
+    sold: true
+}
+
+console.log(car)
+console.log(car["company"])
+console.log(car.model)
+
+var person = {
+    name: "Sayed",
+    age: 2023
+}
+
+console.log("Hello " + person.name + " your age is " + person.age)
+
+/////////////////////////////////////////////////////////////////////////////
+function firstFn() {
+    console.log("This is the first function in JS course")
+}
+
+firstFn()
+
+function secondFn(x = 0, y = 0) {
+    return x + y
+}
+
+console.log(secondFn(8, 5))
+console.log(secondFn(8, 50))
+console.log(secondFn())
+
+console.log(8 * "A")
 
 
-// One line comment
-
-/*   Multi line comment */
-
-
+function sum(x, y) {
+    return x + y
+}
+function sub(x, y) {
+    return x - y
+}
+function multi(x, y) {
+    return x * y
+}
+function division(x, y) {
+    return x / y
+}
 /*
-var x;
+var x = +prompt("Please enter your first number"),
+    y = +prompt("Please enter your second number")
 
-x = 50;
+console.log(sum(x, y))
+console.log(sub(x, y))
+console.log(multi(x, y))
+console.log(division(x, y))
 */
-var x = 50,
-    y = 60,
-    z = x + y;
 
-console.log(z);
+// function sum2() {
+//     var x = +prompt("Please enter your first number"),
+//         y = +prompt("Please enter your second number");
+//     console.log(x + y)
+// }
 
-
-
-
-
-// String
-var username = "Mohamed Ahmed";
-
-console.log(username)
-
-//Number
-var user_id = 60;
-
-console.log(user_id)
-
-// Boolean
-var valid = true;
-
-console.log(valid)
-
-// Undefined
-
-var q;
-
-console.log(q)
-
-var division = document.getElementById("test");
-
-console.log(division)
+// sum2()
 
 
-/***************************************/
+var employees = ["Mohamed", "Mahmoud", "Omar", "Khaled"];
 
-var firstName = "Osman",
-    lastName = "Ali";
+employees[4] = "Ahmed";
 
-console.log(firstName + "     " + lastName);
+console.log(employees)
+console.log(typeof employees)
+console.log(employees.length)
+console.log(employees[0])
+console.log(employees.at(-1))
+
+var x = 500;
+
+if (x > 4) {
+    console.log("x GT Four")
+
+    if (x > 100) {
+        console.log("X > 100 & X > 4")
+    }
+}
+else if (x < 4) {
+    console.log("x LT Four")
+}
+else {
+    console.log("x Eq 4")
+}
+console.log("*****************")
+
+var r = 20;
+
+console.log(r)
+
+// r = r + 1;
+
+console.log(++r)
+
+console.log(r--)
+
+console.log(r)
 
 
-/*************************************
+console.log(1378523 % 2)
 
-var firstName2 = +prompt("Please enter your first name"),
-    lastName2 = +prompt("Please enter your last name")
+var t = 40;
 
-console.log(firstName2 + lastName2)
+console.log(t)
+
+t += 7;
+
+console.log(t)
 
 
-var age = prompt("Please enter your age");
 
-console.log("Your age in days is : "+age * 365)
-**/
+console.log(5 && null && 4)
+
+// & returns first false and last true
+
+console.log(5 || 4)
+
+// | returns first true and last false
