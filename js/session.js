46a7e303d3c89ccb0b216284cdba34786b2198c2
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-09-04 19:09:36 +0300

    Bootstrap

--- js/session.js
+++ js/session.js
@@ -1,46 +1,16 @@
-// function helloFn() {
-//     console.log("Hello")
-// }
+var persons = ["Ahmed", "Khaled", "no", "Omar", "Mahmoud"];
 
-// var helloInterval = setInterval(helloFn, 500)
+// var indexOfNo = persons.indexOf("no");
 
-// function stopHello() {
-//     clearInterval(helloInterval)
-// }
+// persons.fill("Thanks", indexOfNo + 1)
+
+// console.log(persons)
 
 
-// function hello2() {
-//     console.log("Hello from time out")
-// }
+var indexOfNo;
 
-// var helloTimOut = setTimeout(hello2, 3000)
-
-
-// function stopHello2() {
-//     clearTimeout(helloTimOut)
-// }
-
-var persons = ["Ahmed", "Ibrahim", "Khaled"]
-
-console.log(Math.round(3.6))
-console.log(Math.ceil(2.1))
-console.log(Math.floor(3.9))
-console.log(Math.max(5, 1, 7, 815, 2))
-console.log(Math.min(5, 1, 7, 815, 2))
-console.log(Math.random() * 4)
-console.log(Math.round(Math.random() * 100))
-console.log(Math.abs(-8))
-console.log(Math.pow(5, 2))
-
-
-///////////////////////////////////////////////
-console.log(Number.MAX_VALUE)
-console.log(Number.MAX_SAFE_INTEGER)
-console.log(Number.MIN_VALUE)
-console.log(Number.MIN_SAFE_INTEGER)
-console.log(Number.NEGATIVE_INFINITY)
-console.log(isNaN("dfgh"))
-
-var x = 2.48591484;
-console.log(x.toFixed(3))
-console.log(x.toPrecision(3))
+for (let index = 0; index < persons.length; index++) {
+    const element = persons[index];
+    if (element === "no") { }
+}
+console.log(indexOfNo)
