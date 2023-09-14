67bd5e92d649107d49a902e33f8b014689fa1187
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-09-14 12:51:05 +0300

    Add and remove attributes

--- js/session.js
+++ js/session.js
@@ -1,26 +1,59 @@
-console.log(document.body)
-console.log(document.characterSet)
+var username_div = document.getElementById("username")
 
-var usernameDiv = document.getElementById("username")
+console.log(username_div.getAttribute("data-custom"))
 
-console.log(usernameDiv)
 
-var usersDiv = document.getElementsByClassName("users")
+function set_custom_attr() {
+    username_div.setAttribute("fromJS", "This value from JS file")
+}
 
-console.log(usersDiv)
+console.log(username_div.innerHTML)
+console.log(username_div.outerHTML)
+console.log(username_div.textContent)
 
-var contactInfoDiv = document.getElementsByName("contactInfo");
 
-console.log(contactInfoDiv)
+var username_input = document.querySelector("#usernameInput"),
+    output = document.getElementById("output")
 
-var divisionSelector = document.getElementsByTagName("div");
 
-console.log(divisionSelector)
 
-var selectOne = document.querySelector("#username");
+function get_input_value() {
+    // console.log(username_input)
+    // console.log(username_input.value)
+    output.innerHTML = username_input.value
+}
 
-console.log(selectOne)
+var output2 = document.getElementById("output2");
 
-var selectMore = document.querySelector("section .users");
+function create_elements_fn() {
+    var my_element = document.createElement("p"),
+        element_text = document.createTextNode("This is text"),
+        my_comment = document.createComment("This is comment"),
+        element_child = document.createElement("span");
 
-console.log(selectMore)
+    my_element.appendChild(element_child)
+    my_element.append(element_text)
+    my_element.prepend(my_comment)
+    // my_element.style.background = "#F00"
+    my_element.className = "test"
+
+    output2.appendChild(my_element)
+}
+
+var my_images = document.images,
+    my_forms = document.forms;
+
+console.log(my_images)
+console.log(my_forms[0].children)
+console.log(my_forms[0].childNodes)
+
+/////////////////////////////////////////////////////////
+var my_form = document.getElementById("myForm"),
+    results = []
+function show_values() {
+    for (let index = 0; index < my_form.children.length; index++) {
+        const element = my_form.children[index];
+        results.push(element.value)
+    }
+    console.log(results)
+}
