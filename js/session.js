07a67ef6dd01536e637c5ae9c2c80574490694db
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-09-21 13:30:51 +0300

    JS BOM , json and Ajax

--- js/session.js
+++ js/session.js
@@ -1,89 +1,74 @@
-var first = document.getElementById("first"),
-    parent = document.getElementById("parent");
-
-
-
-
-function addClass() {
-    // first.className = "fromJS"
-    // first.classList.add("fromJs")
-    // first.classList.remove("fromHTML")
-    // first.classList.toggle("fromJs")
-    // console.log(first.classList.contains("fromHTML"))
-    // console.log(first.classList.item(1))
-    parent.innerHTML += `
-        <div class="Ok">
-            This is from JS
-            <span>This is span from js</span>
-            <input type="password" name="educationPass"/>
-        </div>
-        `
-}
-
-
-console.log(parent.firstElementChild)
-console.log(parent.lastElementChild)
-console.log(parent.lastChild)
-
-var tagFromJS = document.createElement("span");
-
-tagFromJS.append("This is span from JS")
-
-parent.insertBefore(tagFromJS, parent.children[2])
-
-parent.removeChild(document.getElementsByClassName("test")[3])
-
-console.log(parent.children[2].nextElementSibling)
-console.log(parent.children[2].previousElementSibling)
-
-
-
-/////////////////////////////////////////////////////////////
-//  EVENTS
-var btn = document.querySelector("#btn")
-
-// btn.onclick = function () {
-//     console.log("Hello")
-// }
-
-// btn.onclick = function () {
-//     console.log("Hello 2")
-// }
-
-
-
-// btn.addEventListener("click", function () {
-//     console.log("Hello from listener 2")
-// })
-
-
-
-btn.addEventListener("click", function () {
-    console.log("Hello from listener")
-})
-
-window.onload = function () { console.log("Loaded") }
-
-
-
-var myInput = document.getElementById("myInput"),
-    myOutput = document.querySelector("#output");
-
-myInput.addEventListener("blur", function (ev) {
-    // myOutput.innerHTML = ev.target.value
-    console.log("blur")
-})
 
 /**************************** To do list*******************************/
 
 var myForm = document.forms[0],
-    myTaskInput = myForm.children[0],
-    myOutput = document.getElementById("tasksOutput");
+    myTaskInput = document.getElementById("taskInput"),
+    myOutput = document.getElementById("tasksOutput"),
+    tasksCounterOutput = document.querySelector("#tasks_counter"),
+    finishedTasksCounter = document.querySelector("#finished_tasks_counter");
 
 
 myForm.addEventListener("submit", function (ev) {
     ev.preventDefault();
-    myOutput.innerHTML += `<li>${myTaskInput.value}</li>`;
+    if (myTaskInput.value.trim() === "") {
+        alert("You can't add an empty task")
+        afterAdd()
+    }
+    else {
+        addNewTask();
+        afterAdd();
+        finishTask();
+        deleteTask();
+        tasksCounterFn();
+    }
+})
+
+function addNewTask() {
+    myOutput.innerHTML += `
+        <li class="list-group-item d-flex justify-content-between">
+            ${myTaskInput.value}
+            <div class="control">
+                <i class="m-2 fa-solid fa-flag-checkered text-success"></i>
+                <i class="m-2 fa-solid fa-delete-left text-danger"></i>
+            </div>
+        </li>`;
+}
+
+function afterAdd() {
     myTaskInput.value = "";
     myTaskInput.focus();
-})
+}
+
+function finishTask() {
+    var finish_icons = document.getElementsByClassName("fa-flag-checkered");
+
+    for (let index = 0; index < finish_icons.length; index++) {
+        const finish_icon = finish_icons[index];
+        finish_icon.addEventListener("click", function () {
+            finish_icon.parentElement.parentElement.classList.toggle("finished")
+            finishedTasksCounterFn()
+        })
+    }
+}
+
+function deleteTask() {
+    var delete_icons = document.getElementsByClassName("fa-delete-left");
+
+    for (let index = 0; index < delete_icons.length; index++) {
+        const delete_icon = delete_icons[index];
+        delete_icon.addEventListener("click", function () {
+            delete_icon.parentElement.parentElement.remove()
+            tasksCounterFn()
+            finishedTasksCounterFn()
+        })
+    }
+}
+
+function tasksCounterFn() {
+    tasksCounterOutput.innerHTML = myOutput.children.length
+}
+function finishedTasksCounterFn() {
+    var finishedTasks = document.getElementsByClassName("finished");
+
+    finishedTasksCounter.innerHTML = finishedTasks.length
+}
